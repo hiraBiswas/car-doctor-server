8e13608432b5d4d77cf2c70d37b3436cc76b8fc0
hiraBiswas <hbhirabiswas@gmail.com>
2023-12-16 15:30:12 +0600

    admin varified

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-const express = require('express')
-const cors = require('cors')
-const jwt= require('jsonwebtoken')
-const cookieParser= require('cookie-parser')
-const app = express()
-const port = process.env.PORT || 5900;
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config()
+const jwt = require('jsonwebtoken');
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+  
+app.use(express.json());
+
+console.log(process.env.DB_USER)
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require("dotenv").config()
+const uri = "mongodb+srv://delivery_service:vJ99SbNGNQpc9cEf@cluster0.eogwfq1.mongodb.net/?retryWrites=true&w=majority";
+// const uri = `mongodb+srv://${encodeURIComponent(process.env.DB_USER)}:${encodeURIComponent(process.env.DB_PASS)}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
 
 
-app.use(cors({
-  origin: ['http://localhost:5173'],
-  credentials : true
-}))
-app.use(express.json())
-app.use(cookieParser())
-
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eogwfq1.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -28,135 +25,124 @@ const client = new MongoClient(uri, {
   }
 });
 
-// middlewares
-const logger = async (req, res, next) => {
-  console.log('called:', req.host, req.originalUrl)
-  next();
-}
-
-const verifyToken = async (req, res, next) => {
-  const token = req.cookies?.token;
-  console.log('token from middleware', token)
-  if (!token) {
-      return res.status(401).send({ message: 'unauthorized access' })
-  }
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-      if (err) {
-        console.log('error from jwt', err)
-          return res.status(401).send({ message: 'unauthorized access' })
-      }
-      console.log('value in the token', decoded)
-      req.user = decoded;
-      next();
-  })
-}
-
-
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+     // Connect the client to the server	(optional starting in v4.7)
+   
 
-   const serviceCollection = client.db('carDoctor').collection('services')
-   const bookingsCollection = client.db('carDoctor').collection('bookings')
+     const userCollection = client.db("truckNow").collection("users");
     
-   //auth related 
-   app.post('/jwt',logger, async(req,res) =>{
+     
+
+   // jwt related api
+   app.post('/jwt', async (req, res) => {
     const user = req.body;
-    console.log(user);
     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-    res.cookie('token', token, {
-      httpOnly: true,
-      secure: true,
-    });
-    res.send({ success: true });
-    
-   })
+    res.send({ token });
+  })
 
 
-
-   app.get('/services',logger, async(req, res)=>{
-   const cursor = serviceCollection.find()
-   const result = await cursor.toArray()
-   res.send(result);
-   })
-
-   app.get('/services/:id', async(req, res)=>{
-    const id = req.params.id;
-    const query = {_id: new ObjectId(id)}
-
-    const options = {
-      
-      projection: { price:1, title: 1, img:1, service_id:1 },
-
-    };
-
-    const result = await serviceCollection.findOne(query,options);
-    res.send(result)
-   })
-   
-
-   app.get('/bookings',logger,verifyToken, async(req, res)=>{
-    console.log(req.query.email)
-    console.log('user in the valid token', req.user)
-    if(req.query.email !== req.user.email){
-      return res.status(403).send({message: 'Access Forbidden'})
+  // middlewares 
+  const verifyToken = (req, res, next) => {
+    // console.log('inside verify token', req.headers.authorization);
+    if (!req.headers.authorization) {
+      return res.status(401).send({ message: 'unauthorized access' });
     }
-    let query = {};
-    if(req.query?.email){
-      query = {email: req.query.email}
+    const token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        return res.status(401).send({ message: 'unauthorized access' })
+      }
+      req.decoded = decoded;
+      next();
+    })
+  }
+
+
+   // use verify admin after verifyToken
+   const verifyAdmin = async (req, res, next) => {
+    const email = req.decoded.email;
+    const query = { email: email };
+    const user = await userCollection.findOne(query);
+    const isAdmin = user?.type === 'admin';
+    if (!isAdmin) {
+      return res.status(403).send({ message: 'forbidden access' });
     }
-    const result = await bookingsCollection.find(query).toArray()
-    res.send(result)
-   })
+    next();
+  }
 
+  // users related api
+  app.get('/users', verifyToken, verifyAdmin, async (req, res) => {
+    const result = await userCollection.find().toArray();
+    res.send(result);
+  });
 
-   app.post('/bookings', async(req, res)=>{
-    const booking = req.body;
-            console.log(booking);
-            const result = await bookingsCollection.insertOne(booking);
-            res.send(result);
-   })
+  app.get('/users/admin/:email', verifyToken, async (req, res) => {
+    const email = req.params.email;
 
-   
+    if (email !== req.decoded.email) {
+      return res.status(403).send({ message: 'forbidden access' })
+    }
 
+    const query = { email: email };
+    const user = await userCollection.findOne(query);
+    let admin = false;
+    if (user) {
+      admin = user?.type === 'admin';
+    }
+    res.send({ admin });
+  })
 
-   app.delete('/bookings/:id', async(req, res)=>{
-    const id = req.params.id;
-    const query = {_id: new ObjectId(id)}
-    const result = await bookingsCollection.deleteOne(query)
-    res.send(result)
-   })
+  app.post('/users', async (req, res) => {
+    const user = req.body;
+    // insert email if user doesnt exists: 
+    // you can do this many ways (1. email unique, 2. upsert 3. simple checking)
+    const query = { email: user.email }
+    const existingUser = await userCollection.findOne(query);
+    if (existingUser) {
+      return res.send({ message: 'user already exists', insertedId: null })
+    }
+    const result = await userCollection.insertOne(user);
+    res.send(result);
+  });
 
-   app.patch('/bookings/:id', async (req, res) => {
+  app.patch('/users/admin/:id', verifyToken, verifyAdmin, async (req, res) => {
     const id = req.params.id;
     const filter = { _id: new ObjectId(id) };
-    const updatedBooking = req.body;
-    console.log(updatedBooking);
-    const updateDoc = {
-        $set: {
-            status: updatedBooking.status
-        },
-    };
-    const result = await bookingsCollection.updateOne(filter, updateDoc);
+    const updatedDoc = {
+      $set: {
+        type: 'admin'
+      }
+    }
+    const result = await userCollection.updateOne(filter, updatedDoc);
     res.send(result);
-})
+  })
 
+  app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await userCollection.deleteOne(query);
+    res.send(result);
+  })
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
+     // Send a ping to confirm a successful connection
+     await client.db("admin").command({ ping: 1 });
+     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  
+
+  }
+  finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
 
-app.get('/',(req,res)=>{
-res.send('doctor is running')
+
+app.get('/', (req, res) => {
+  res.send('TruckNow is connecting')
 })
 
-app.listen(port,()=>{
-    console.log(`car doctor is running on port ${port}`)
+app.listen(port, () => {
+  console.log(`TruckNow is sitting on port ${port}`);
 })
